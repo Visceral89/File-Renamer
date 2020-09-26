8108e31bf63100e1cd43bd02d40903372ade7f31
Rasmus <39956903+Visceral89@users.noreply.github.com>
2020-09-26 22:24:06 +0200

    Rough idea scetching, trying out some initial code.

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 # Batch File Renamer for easier processing for Plex
 # Made by Rasmus Bremholm 2020
+# Good starting point https://www.geeksforgeeks.org/rename-multiple-files-using-python/
+
+import os #Here we are importing OS library
+
+def main():
+
+    for count, filename in enumerate(os.listdir("xyz")): #getting the files in specified directory
+        dst = prefix + str(count) + ".jpg"
+        src = xyz + filename
+        dst = xyz + dst
+
+        os.rename(src, dst)
+
+if __name__ =="__main__":
+    main()
