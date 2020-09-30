5ac0965c3fbd57da38ced1fae37e91a8d9de8937
Rasmus <39956903+Visceral89@users.noreply.github.com>
2020-09-30 20:33:45 +0200

    Major reworks of TK, changed it to a grid based system, also experimententing with the main function of renaming files.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Batch File Renamer for easier processing for Plex
 # Made by Rasmus Bremholm 2020
-# Good starting point https://www.geeksforgeeks.org/rename-multiple-files-using-python/
+# Good starting point https://www.geeksforgeeks.org/rename-all-file-names-in-your-directory-using-python/?ref=lbp
+# http://timgolden.me.uk/python/win32_how_do_i/browse-for-a-folder.html
 
 
 # Here we are importing OS library
 import os
 import tkinter as tk
-import requests
 
 height = 600
 width = 800
@@ -14,15 +14,16 @@ width = 800
 input_dir = ""
 output_dir = ""
 count = 1
-directory = ""
-suffix = ""
+prefix = ""
+index = 0
 
 # Setting up the root window
 root = tk.Tk()
+root.title("File Renamer by Rasmus")
 
-# main container
-canvas = tk.Canvas(root, height=height, width=width)
-canvas.pack()
+# main grid
+e = tk.Entry(root, width=35, borderwidth=5)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 # content container
 frame = tk.Frame(root)
@@ -30,18 +31,13 @@ frame.place(relx=0.05, rely=0.05, relwidth=0.95, relheight=0.95)
 
 root.mainloop()
 
-os.chdir(directory)
-
-
-def increment():
-    global count
-    count = count + 1
-
+os.chdir("G://3D")
+print(os.getcwd())
 
 for file in os.listdir():
     file_name, file_ext = os.path.split(file)
-    file_name = suffix + str(count)
-    increment()
+    file_name = prefix + str(index)
 
     new_name = "{} {}".format(file_name, file_ext)
     os.rename(file, new_name)
+    print(file_name)
